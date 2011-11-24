1ea3642afa8d26106643c17110b06e0cb31d1c9e
AJ00200 <aj0020@aj00200.org>
2011-11-24 09:48:44 -0600

    Minor changes to the storage code. Added a place to store USK and UUK blocks.

--- src/libs/storage/manager.py
+++ src/libs/storage/manager.py
@@ -3,9 +3,26 @@ information that has not been requested in a long time.
 TODO: decide on how to store the data long term (persistent storage).
 '''
 import hashlib
+import libs.errors
+import libs.events
 
 class Block(object):
+    max_size = 2048 # 2048 for now because of the RSA Crypto limit
     def __init__(self, data):
+        if data > self.max_size:
+            raise libs.errors.AnonError('Block too big.')
         self.data = data
         self.hash = hashlib.sha256(data).hexdigest()
         # TODO: Decide on using sha256 or sha1 (or even sha512)^
+        
+class StorageDB(libs.events.Handler):
+    '''An database type object to store and sort blocks.'''
+    def __init__(self):
+        self.usks = {}
+        self.uuks = {}
+        
+    def add_usk(self, block):
+        self.usks[Block.hash] = Block
+        
+    def add_uuk(self, block):
+        self.uuks[Block.hash] = Block
